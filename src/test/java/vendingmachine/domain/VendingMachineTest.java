45b7a1ae2074b281c63619d002523d5eb985c5e1
HiiWee <wpdlzhf159@gmail.com>
2022-12-16 21:08:23 +0900

    test: 현재 금액으로 구매할 수 있는 상품이 없을때 예외 발생 테스트

--- src/test/java/vendingmachine/domain/VendingMachineTest.java
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import vendingmachine.domain.enums.Coin;
+import vendingmachine.exception.NoPossibleProductToBuyException;
 import vendingmachine.message.ErrorMessage;
 
 class VendingMachineTest {
@@ -20,5 +21,20 @@ class VendingMachineTest {
         Assertions.assertThatThrownBy(() -> vendingMachine.putMoney(99))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_USER_MONEY);
+    }
+
+    @DisplayName("현재 금액이 부족할때 에외 발생 테스트")
+    @Test
+    void validateMachineStatus_notEnoughMoney_exception() {
+        // given
+        VendingMachine vendingMachine = new VendingMachine(new Coins(List.of(Coin.COIN_500)),
+                new Products(List.of(new Product(List.of("a", "200", "20")))));
+
+        // when
+        vendingMachine.putMoney(100);
+
+        // then
+        Assertions.assertThatThrownBy(vendingMachine::validateMachineStatus)
+                .isInstanceOf(NoPossibleProductToBuyException.class);
     }
 }
