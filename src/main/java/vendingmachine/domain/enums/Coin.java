79c311df3ddb63e5e661286770ec596f1763b144
HiiWee <wpdlzhf159@gmail.com>
2022-12-16 17:29:32 +0900

    feat: 동전 랜덤 생성시 필요 메소드 정의

--- src/main/java/vendingmachine/domain/enums/Coin.java
+++ src/main/java/vendingmachine/domain/enums/Coin.java
@@ -1,5 +1,10 @@
 package vendingmachine.domain.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import vendingmachine.message.ErrorMessage;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -12,5 +17,22 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public static List<Integer> getMoneyList() {
+        return Arrays.stream(values())
+                .map(coin -> coin.amount)
+                .collect(Collectors.toList());
+    }
+
+    public static Coin getContainsCoin(final int value) {
+        return Arrays.stream(values())
+                .filter(coin -> coin.amount == value)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_COIN));
+    }
+
+    public static int convertCoinToSumAmount(final List<Coin> coins) {
+        return coins.stream()
+                .mapToInt(coin -> coin.amount)
+                .sum();
+    }
 }
