4abb3c4919ec30e3dcfcfa0e3242b7df54fa2644
HiiWee <wpdlzhf159@gmail.com>
2022-12-16 20:06:16 +0900

    feat: 입력한 상품 가격, 수량에 대한 예외처리 기능 추가

--- src/main/java/vendingmachine/message/ErrorMessage.java
+++ src/main/java/vendingmachine/message/ErrorMessage.java
@@ -4,6 +4,10 @@ public class ErrorMessage {
     public static final String INVALID_NUMERIC = "[ERROR] 숫자만 입력해야 합니다.";
     public static final String INVALID_MACHINE_PRICE = "[ERROR] 자판기 금액은 10원 단위로 입력해야 합니다.";
     public static final String INVALID_COIN = "[ERROR] 변환할 수 있는 동전이 없습니다.";
+    public static final String INVALID_PRICE_NUMERIC = "[ERROR] 상품 가격은 숫자만 입력해야 합니다.";
+    public static final String INVALID_PRODUCT_MEASURE = "[ERROR] 상품 가격은 10원 단위로 나누어 떨어져야 합니다.";
+    public static final String INVALID_PRODUCT_PRICE = "[ERROR] 상품 가격은 100원 이상이어야 합니다.";
+    public static final String INVALID_QUANTITY_NUMERIC = "[ERROR] 상품 수량은 숫자마 입력해야 합니다.";
 
 
     private ErrorMessage() {
